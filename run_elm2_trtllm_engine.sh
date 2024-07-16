17bd315e425d50b635b0aecd7cb2cbd0ac680fd8
chinnadhurai.sankar <chinnadhurai@slicex.ai>
2024-07-16 04:34:39 +0000

    update

--- run_elm2_trtllm_engine.sh
+++ run_elm2_trtllm_engine.sh
@@ -1,14 +1,12 @@
+#!/bin/bash
 
-
-CURR_FOLDER=$(pwd)
-DOCKER_NAME=elm_trtllm
 
 if [ $# -ne 3 ]; then
-    echo "Usage: $0 <elm2_model_id> <gpu_type> <input_prompt>"
+    echo "Usage: $0 <elm2_model_id> <gpu_type> '<input_prompt>'"
     echo "Example command to run A100 engine: $0 slicexai/elm2-0.50-instruct A100 'Can you provide ways to eat combinations of bananas and dragonfruits?'"
-    echo "Example command to run H100 engine: $0 slicexai/elm2-0.50-instruct H100"
-    echo "Supported 'elm2_model_id' choices : [elm2-0.50-instruct, elm2-0.25-instruct, elm2-0.125-instruct]"
-    echo "Supported GPU types : [A100, H100]"
+    echo "Example command to run H100 engine: $0 slicexai/elm2-0.50-instruct H100 'Can you provide ways to eat combinations of bananas and dragonfruits?'"
+    echo "Supported 'elm2_model_id' choices : [slicexai/elm2-0.50-instruct, slicexai/elm2-0.25-instruct, slicexai/elm2-0.125-instruct]"
+    echo "Supported gpt_types : [A100, H100]"
     exit 1
 fi
 
@@ -23,8 +21,8 @@ echo "PROMPT:"$PROMPT
 ENGINE_DIR="${ELM2_HF_MODEL_DIR}-trtllm-${GPU_TYPE}"
 
 cd /lm/TensorRT-LLM/examples
-huggingface-cli download slicexai/${ENGINE_DIR} --local-dir ${ENGINE_DIR}
-huggingface-cli download slicexai/${ELM2_HF_MODEL_DIR} --local-dir ${ELM2_HF_MODEL_DIR}
+huggingface-cli download ${ENGINE_DIR} --local-dir ${ENGINE_DIR}
+huggingface-cli download ${ELM2_HF_MODEL_DIR} --local-dir ${ELM2_HF_MODEL_DIR}
 
 python3 run.py \
   --engine_dir ${ENGINE_DIR} \
