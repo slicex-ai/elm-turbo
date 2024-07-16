68cd5465ae54678d668333dc838771fa54aec15b
chinnadhurai <120986639+chinnadhuraisankar@users.noreply.github.com>
2024-07-15 21:27:53 -0700

    Update setup_trtllm.sh

--- setup_trtllm.sh
+++ setup_trtllm.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+#Based on installation instructions provided at --> https://nvidia.github.io/TensorRT-LLM/installation/linux.html#installing-on-linux
+
 rm -rf TensorRT-LLM
 git clone https://github.com/NVIDIA/TensorRT-LLM.git
-#cd TensorRT-LLM
 
 CURR_FOLDER=$(pwd)
 DOCKER_NAME=elm_trtllm
@@ -10,11 +11,11 @@ DOCKER_IMAGE=nvidia/cuda:12.4.0-devel-ubuntu22.04
 
 docker run -d --gpus all -it --shm-size=8g --name ${DOCKER_NAME} --ulimit memlock=-1 --rm -v ${CURR_FOLDER}:/lm $DOCKER_IMAGE
 
-command1="cd /lm && apt-get update && apt-get -y install python3.10 python3-pip openmpi-bin libopenmpi-dev"
-docker exec ${DOCKER_NAME} sh -c "${command1}"
+command="cd /lm && apt-get update && apt-get -y install python3.10 python3-pip openmpi-bin libopenmpi-dev"
+docker exec ${DOCKER_NAME} sh -c "${command}"
 
-command1="pip3 install tensorrt_llm -U --pre --extra-index-url https://pypi.nvidia.com"
-docker exec ${DOCKER_NAME} sh -c "${command1}"
+command="pip3 install tensorrt_llm -U --pre --extra-index-url https://pypi.nvidia.com"
+docker exec ${DOCKER_NAME} sh -c "${command}"
 
-command1="python3 -c 'import tensorrt_llm'"
-docker exec ${DOCKER_NAME} sh -c "${command1}"
+command="python3 -c 'import tensorrt_llm'"
+docker exec ${DOCKER_NAME} sh -c "${command}"
