cc2b091aaa7351c2d10099bfcb3b8a1c097af6c2
chinnadhurai.sankar <chinnadhurai@slicex.ai>
2024-07-16 05:03:24 +0000

    update

--- run_elm2_trtllm_engine.sh
+++ run_elm2_trtllm_engine.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+set -e
 
 if [ $# -ne 3 ]; then
     echo "Usage: $0 <elm2_model_id> <gpu_type> '<input_prompt>'"
-    echo "Example command to run A100 engine: $0 slicexai/elm2-0.50-instruct A100 'Can you provide ways to eat combinations of bananas and dragonfruits?'"
-    echo "Example command to run H100 engine: $0 slicexai/elm2-0.50-instruct H100 'Can you provide ways to eat combinations of bananas and dragonfruits?'"
+    echo "Example command to run A100 engine: $0 slicexai/elm2-0.50-instruct A100 'plan a fun day with my grandparents.'"
+    echo "Example command to run H100 engine: $0 slicexai/elm2-0.50-instruct H100 'plan a fun day with my grandparents.'"
     echo "Supported 'elm2_model_id' choices : [slicexai/elm2-0.50-instruct, slicexai/elm2-0.25-instruct, slicexai/elm2-0.125-instruct]"
     echo "Supported gpt_types : [A100, H100]"
     exit 1
@@ -26,9 +26,12 @@ huggingface-cli download ${ELM2_HF_MODEL_DIR} --local-dir ${ELM2_HF_MODEL_DIR}
 
 python3 run.py \
   --engine_dir ${ENGINE_DIR} \
-  --max_output_len 128 \
+  --max_output_len 512 \
+  --presence_penalty 0.7 \
+  --frequency_penalty 0.7 \
+  --temperature 0.0 \
   --tokenizer_dir ${ELM2_HF_MODEL_DIR} \
   --input_text """<s><|user|>
-${PROMPT}<|end|>
+  ${PROMPT}<|end|>
 <|assistant|>
 """
